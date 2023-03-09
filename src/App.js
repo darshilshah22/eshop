2110883602e21fa294caed5a6c99dcae55c9e219
Darshil <darshil@zealousweb.com>
2023-03-09 17:14:11 +0530

    product listing page completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Carousel from "./components/Carousel/Carousel";
-import Category from "./components/ChoiceCategory/Category";
-import TodaysDeal from "./components/TodaysDeal/TodaysDeal";
-import {data} from "./components/constants";
-import Services from "./components/Services/Services";
-import TopBrands from "./components/TopBrands/TopBrands";
-import Recommends from "./components/Recommends/Recommends";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
 
 function App() {
   return (
     <>
-      <Carousel />
-      <TodaysDeal title= "Today's Deals" isEnd="true" deals={data.dailyDeals}/>
-      <Category cates={data.cates}/>
-      <TodaysDeal title= "Deals of the Month" deals={data.monthlyDeals}/>
-      <Services />
-      <TopBrands />
-      <Recommends />
+      {/* <Home /> */}
+      <Products />
     </>
   );
 }
--- src/components/TodaysDeal/TodaysDeal.js
+++ src/components/TodaysDeal/TodaysDeal.js
@@ -1,17 +1,73 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Dealcards from "../Dealcards/Dealcards";
 import "./todaysDeal.css";
 
-const TodaysDeal = ({title, isEnd, deals}) => {
+const TodaysDeal = ({ title, isEnd, deals }) => {
+  const Ref = useRef(null);
+
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const getTimeRemaining = (e) => {
+    const total = Date.parse(e) - Date.parse(new Date());
+    setSeconds(Math.floor((total / 1000) % 60));
+    setMinutes(Math.floor((total / 1000 / 60) % 60));
+    setHours(Math.floor((total / 1000 / 60 / 60) % 24));
+    return {
+      total,
+      hours,
+      minutes,
+      seconds,
+    };
+  };
+
+  const startTimer = (e) => {
+    let { total, hours, minutes, seconds } = getTimeRemaining(e);
+  };
+
+  const clearTimer = (e) => {
+    const id = setInterval(() => {
+      startTimer(e);
+    }, 1000);
+    Ref.current = id;
+  };
+
+  const getDeadTime = () => {
+    const today = new Date();
+    let deadline = new Date();
+    deadline.setHours(deadline.getHours() + (23 - deadline.getHours()));
+    deadline.setMinutes(deadline.getMinutes() + (60 - deadline.getMinutes()));
+    deadline.setSeconds(deadline.getSeconds() + (60 - deadline.getSeconds()));
+
+    return deadline;
+  };
+
+  useEffect(() => {
+    onClickReset();
+  }, []);
+
+  const onClickReset = () => {
+    clearTimer(getDeadTime());
+  };
+
   return (
     <>
       <div className="title">
         <p className="todaysdeal">{title}</p>
-        {isEnd ? <p className="endsin">Ends in <span>04</span> : <span>24</span> : <span>39</span></p> : <></>}
+        {isEnd ? (
+          <p className="endsin">
+            Ends in <span>{hours > 9 ? hours : "0" + hours}</span> :{" "}
+            <span>{minutes > 9 ? minutes : "0" + minutes}</span> :{" "}
+            <span>{seconds > 9 ? seconds : "0" + seconds}</span>
+          </p>
+        ) : (
+          <></>
+        )}
       </div>
       <div className="deal-cards">
         {deals?.map((deal) => (
-          <Dealcards deall={deal}/>
+          <Dealcards deall={deal} />
         ))};
       </div>
     </>
