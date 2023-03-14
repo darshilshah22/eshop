a87f7bf51337f0ba4fb692237ee9786c0983622b
darrshilshah22 <shah.darshilp@gmail.com>
2023-03-14 22:21:35 +0530

    working pd screen

--- src/components/ProductReview/ProductReview.js
+++ src/components/ProductReview/ProductReview.js
@@ -1,18 +1,30 @@
 import React from "react";
-import "./productReview.css";
+import GradientSVG from "./gradientSVG";
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const ProductReview = () => {
+  const idCSS = "hello";
+  const percentage = 4.5;
   return (
     <div className="container ms-3">
       <p className="m-0 fw-semibold fs-5">Product Review</p>
-      <div class="progress blue">
-        <span class="progress-left">
-          <span class="progress-bar"></span>
-        </span>
-        <span class="progress-right">
-          <span class="progress-bar"></span>
-        </span>
-        <div class="progress-value">50%</div>
+      <div style={{ height: "150px", width: "150px" }}>
+        <GradientSVG />
+        <CircularProgressbarWithChildren
+          strokeWidth={15}
+          value={(percentage/5)*100}
+          styles={{
+            path: { stroke: `url(#${idCSS})`, strokeLinecap: 'butt' },
+            trail: {
+              stroke: "#E2E2E2"
+            },
+          }}
+        >
+          <div style={{ fontSize: "36px", marginTop: -5, color: "black" }}>
+            <strong>{percentage}</strong>
+          </div>
+        </CircularProgressbarWithChildren>
       </div>
     </div>
   );
